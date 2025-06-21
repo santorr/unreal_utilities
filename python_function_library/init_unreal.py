8f5302ae37320100a546c24368dfc09b9743b237
Jeremy duchesne <duchesne.pro@gmail.com>
2025-06-21 17:02:50 +0200

    Update init_unreal.py

--- python_function_library/init_unreal.py
+++ python_function_library/init_unreal.py
@@ -2,7 +2,8 @@ import unreal
 import function_library
 
 """
-Make sure to place this file as well as the module in a Python folder that will be initialized at launch by Unreal Engine.
+Make sure to place this file as well as the module in a Python folder that will be initialized at launch by Unreal Engine (eg. /Game/Python/).
+If you don't do this, Unreal won't be able to recognize your function library, and all the nodes will be broken every time the engine starts.
 https://dev.epicgames.com/documentation/en-us/unreal-engine/scripting-the-unreal-editor-using-python?application_version=5.3#pythonenvironmentandpathsintheunrealeditor
 """
 

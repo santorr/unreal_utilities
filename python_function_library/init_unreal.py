0e8d49822ae76a476343a0b42bf397dd18eefc52
Jeremy duchesne <duchesne.pro@gmail.com>
2025-06-21 16:57:00 +0200

    Update init_unreal.py

--- python_function_library/init_unreal.py
+++ python_function_library/init_unreal.py
@@ -1,5 +1,9 @@
 import unreal
 import function_library
 
+"""
+Make sure to place this file as well as the module in a Python folder that will be initialized at launch by Unreal Engine.
+https://dev.epicgames.com/documentation/en-us/unreal-engine/scripting-the-unreal-editor-using-python?application_version=5.3#pythonenvironmentandpathsintheunrealeditor
+"""
 
 unreal.log("Module loaded")
